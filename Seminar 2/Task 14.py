2bb5d5f3c0d06495de914661510495be1d43bc9a
Irina <ms.irina-98@mail.ru>
2023-03-07 16:24:22 +0300

    done Task 14

--- Seminar 2/Task 14.py
+++ Seminar 2/Task 14.py
@@ -8,3 +8,12 @@ while prod * 2 < num:
     prod = 2 ** degree
     print(prod, end=' ')
     degree += 1
+    
+# Второй способ
+
+number = int(input('Введите предел: '))
+
+i = 1
+while i < number:
+    print(i, end=' ')
+    i *= 2
